efbdd25b16750e3ba3c5bb37a61c646d5f96c624
Ralf Kotulla <kotulla@wisc.edu>
2017-06-19 15:55:07 -0500

    added argparse for command line parsing. Made keyword selection user-configurable rather than hard-coding things for ODI.

--- run_photometry.py
+++ run_photometry.py
@@ -10,6 +10,8 @@ import sqlite3
 
 from create_table import read_colunms_from_param_file
 
+import argparse
+
 
 def ingest_frame(db_connection,
                  filename=None, mjd=0, dateobs=0, skylevel=-1,
@@ -64,22 +66,65 @@ def ingest_frame(db_connection,
 
 if __name__ == "__main__":
 
-    database_fn = sys.argv[1]
-    sex_conf_fn = sys.argv[2]
-    sex_param_fn = sys.argv[3]
-    filelist = sys.argv[4:]
+    #
+    # Handle all command line stuff
+    #
+    parser = argparse.ArgumentParser(
+        description='Run photometry and ingest results into database.')
+    parser.add_argument(
+        'database_fn', type=str, #nargs=1,
+        metavar='data.base',
+        help='database filename')
+    parser.add_argument(
+        'sex_conf_fn', metavar='sex.conf', type=str, #nargs=1,
+        help='SExtractor configuration file')
+    parser.add_argument(
+        'sex_param_fn', metavar='sex.param', type=str, #nargs=1,
+        help='SExtractor parameter filename')
+    parser.add_argument(
+        'filenames', metavar='input.fits', type=str, nargs='+',
+        help='(list of) input FITS filename(s)')
+    parser.add_argument('--mjd', dest='mjd',
+                        default='MJD-OBS', help='FITS keyword for MJD')
+    parser.add_argument('--skylevel', dest='skylevel',
+                        default='SKYLEVEL', help='FITS keyword for background level')
+    parser.add_argument('--filter', dest='filter',
+                        default='FILTER', help='FITS keyword for bandpass filter')
+    parser.add_argument('--exptime', dest='exptime',
+                        default='EXPTIME', help='FITS keyword for exposure time')
+    parser.add_argument('--object', dest='object',
+                        default='OBJECT', help='FITS keyword for object name')
+    parser.add_argument('--aimass', dest='airmass',
+                        default='AIRMASS', help='FITS keyword for airmass')
+    parser.add_argument('--wcs_rms', dest='wcs_rms',
+                        default='WCS_RMS', help='FITS keyword for WCS calibration uncertainty')
+    parser.add_argument('--seeing', dest='seeing',
+                        default='SEEING', help='FITS keyword for seeing')
+    parser.add_argument('--magzero', dest='magzero',
+                        default='PHOTZP_X', help='FITS keyword for photometric zeropoint')
+    parser.add_argument('--errmagzero', dest='errmagzero',
+                        default='PHOTZPSD', help='FITS keyword for photometric zeropoint uncertainty')
+    args = parser.parse_args()
 
-    params = read_colunms_from_param_file(sex_param_fn)
+    #
+    # Read & parse the Sextractor parameter file so we know what datafields
+    # to populate in the database
+    #
+    params = read_colunms_from_param_file(args.sex_param_fn)
     params = [p.lower() for p in params]
 
-    for fn in filelist:
+
+    #
+    # Now run all Sextractors and ingest results
+    #
+    for fn in args.filenames:
 
         catalog_fn = os.path.splitext(fn)[0]+".xcat"
 
         # run seource extractor
         if (not os.path.isfile(catalog_fn)):
             cmd = "sex -c %s -PARAMETERS_NAME %s -CATALOG_NAME %s %s" % (
-                sex_conf_fn, sex_param_fn, catalog_fn, fn
+                args.sex_conf_fn, args.sex_param_fn, catalog_fn, fn
             )
             print cmd
             os.system(cmd)
@@ -88,7 +133,7 @@ if __name__ == "__main__":
         data = numpy.loadtxt(catalog_fn)
 
         # now ingest data into database
-        conn = sqlite3.connect(database_fn)
+        conn = sqlite3.connect(args.database_fn)
         curs = conn.cursor()
 
         hdulist = pyfits.open(fn)
@@ -96,17 +141,17 @@ if __name__ == "__main__":
         frameid = ingest_frame(
             db_connection=conn,
             filename=fn,
-            mjd=hdr['MJD-OBS'],
+            mjd=hdr[args.mjd] if args.mjd in hdr else -1,
             dateobs=0,
-            skylevel=hdr['SKYLEVEL'] if 'SKYLEVEL' in hdr else -1,
-            filtername=hdr['FILTER'] if 'FILTER' in hdr else "",
-            exptime=hdr['EXPMEAS'] if 'EXPMEAS' in hdr else -1,
-            object=hdr['OBJECT'] if 'OBJECT' in hdr else "",
-            airmass=hdr['AIRMASS'] if 'AIRMASS' in hdr else -1,
-            wcs_rms=hdr['WCS_RMS'] if 'WCS_RMS' in hdr else -1,
-            seeing=hdr['SEEING'] if 'SEEING' in hdr else -1,
-            magzero=hdr['PHOTZP_X'] if 'PHOTZP_X' in hdr else 0,
-            magzero_err=hdr['PHOTZPSD'] if 'PHOTZPSD' in hdr else -1,
+            skylevel=hdr[args.skylevel] if args.skylevel in hdr else -1,
+            filtername=hdr[args.filter] if args.filter in hdr else "",
+            exptime=hdr[args.exptime] if args.exptime in hdr else -1,
+            object=hdr[args.object] if args.object in hdr else "",
+            airmass=hdr[args.airmass] if args.airmass in hdr else -1,
+            wcs_rms=hdr[args.wcs_rms] if args.wcs_rms in hdr else -1,
+            seeing=hdr[args.seeing] if args.seeing in hdr else -1,
+            magzero=hdr[args.magzero] if args.magzero in hdr else 0,
+            magzero_err=hdr[args.errmagzero] if args.errmagzero in hdr else -1,
         )
 
         #
@@ -135,11 +180,6 @@ if __name__ == "__main__":
             curs.execute(sql.strip())
             #print sql
             print >>sqllog, sql.strip()
-
-        #sql = 'COMMIT;'
-        #print commit
-        #print >>sqllog, sql.strip()
-        #curs.execute(sql)
 
         conn.commit()
 
